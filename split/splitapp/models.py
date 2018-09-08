3fbf7e20e96187d2425636c0b358016e73651ec1
Rebecca Hong <rzh1@andrew.cmu.edu>
2018-09-08 01:20:45 -0400

    added fields to database

--- split/splitapp/models.py
+++ split/splitapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class User(models.Model):
+  time=models.DateTimeField('time requested')
+  name=models.CharField(max_length=50)
+  phone=models.PositiveIntegerField()
+  def __str__(self):
+    return self.time+self.name+self.phone
+  #returns true if the time has already passed
+  def checkExpired(self):
+    return datetime.datetime.now()>=time
